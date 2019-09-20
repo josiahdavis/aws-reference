8800efae01476d6ca8f943b3d0366f8b146dbea3
Josiah Davis <josiah.j.davis@gmail.com>
2019-09-20 14:00:32 +0800

    Update ubuntu_userdata.sh

--- ubuntu_userdata.sh
+++ ubuntu_userdata.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e -x
-curl -o /home/ubuntu/.tmux.conf https://raw.githubusercontent.com/josiahjdavis/aws-reference/master/.tmux.conf
-curl -o /home/ubuntu/start_tmux.sh https://raw.githubusercontent.com/josiahjdavis/aws-reference/master/start_tmux.sh
+curl -o /home/ubuntu/.tmux.conf https://raw.githubusercontent.com/josiahdavis/aws-reference/master/.tmux.conf
+curl -o /home/ubuntu/start_tmux.sh https://raw.githubusercontent.com/josiahdavis/aws-reference/master/start_tmux.sh
