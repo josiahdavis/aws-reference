54565e41b45012da8db2ec0d18e6fda8b6533b67
Josiah Davis <josiah.j.davis@gmail.com>
2019-09-20 12:02:33 +0800

    Create start_tmux.sh

---
+++ start_tmux.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+tmux new-session -s TRAIN -n editor -d
+tmux split-window -v -p 40 -t TRAIN
+tmux split-window -h -p 55 -t TRAIN:0.1
+tmux send-keys -t TRAIN:0.1 'watch -n0.5 nvidia-smi' C-m
+tmux attach -t TRAIN:0.0
