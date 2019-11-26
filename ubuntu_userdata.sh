7ad03d99c587f29bcfc55c4bb8eb9dede5b4416e
Josiah Davis <josiah.j.davis@gmail.com>
2019-11-26 14:42:03 +0800

    Update ubuntu_userdata.sh

--- ubuntu_userdata.sh
+++ ubuntu_userdata.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e -x
+sudo apt-get install ctags
 curl -o /home/ubuntu/.tmux.conf https://raw.githubusercontent.com/josiahdavis/aws-reference/master/.tmux.conf
 curl -o /home/ubuntu/start_tmux.sh https://raw.githubusercontent.com/josiahdavis/aws-reference/master/start_tmux.sh
