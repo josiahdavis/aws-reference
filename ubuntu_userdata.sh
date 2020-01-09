f1d3907080a8617f26664b0c20c0b3e1e12e1ef9
Josiah Davis <josiah.j.davis@gmail.com>
2020-01-09 13:07:27 +0800

    Update ubuntu_userdata.sh

--- ubuntu_userdata.sh
+++ ubuntu_userdata.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e -x
-sudo apt-get install ctags
+apt-get install ctags
 curl -o /home/ubuntu/.tmux.conf https://raw.githubusercontent.com/josiahdavis/aws-reference/master/.tmux.conf
 curl -o /home/ubuntu/start_tmux.sh https://raw.githubusercontent.com/josiahdavis/aws-reference/master/start_tmux.sh
 pip3 install black
