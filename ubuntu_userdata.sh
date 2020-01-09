52a05d1732fb7fa210650af8e9fbe0602661faf1
Josiah Davis <josiah.j.davis@gmail.com>
2020-01-09 13:39:33 +0800

    Install tree

--- ubuntu_userdata.sh
+++ ubuntu_userdata.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 set -e -x
+apt-get install ctags
+apt install ack
+apt install tree
 curl -o /home/ubuntu/.tmux.conf https://raw.githubusercontent.com/josiahdavis/aws-reference/master/.tmux.conf
 curl -o /home/ubuntu/start_tmux.sh https://raw.githubusercontent.com/josiahdavis/aws-reference/master/start_tmux.sh
 pip3 install black
 git clone https://github.com/VundleVim/Vundle.vim.git /home/ubuntu/.vim/bundle/Vundle.vim
 curl -o /home/ubuntu/.vimrc https://raw.githubusercontent.com/josiahdavis/aws-reference/master/.vimrc
-apt-get install ctags
-apt install ack
 chown -R ubuntu:ubuntu /home/ubuntu/.vim
 chown ubuntu:ubuntu /home/ubuntu/.vimrc
 runuser -l  ubuntu -c 'vim +PluginInstall +qall'
