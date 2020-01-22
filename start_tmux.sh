e7072eaa811c15c88c1c6dd26572c3dbc5463481
Josiah Davis <josiah.j.davis@gmail.com>
2020-01-22 14:52:02 +0800

    Update start_tmux.sh

--- start_tmux.sh
+++ start_tmux.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 tmux new-session -s TRAIN -n editor -d
 tmux split-window -v -p 40 -t TRAIN
-tmux split-window -h -p 55 -t TRAIN:0.1
-tmux send-keys -t TRAIN:0.1 'watch -n0.5 nvidia-smi' C-m
+tmux split-window -h -p 65 -t TRAIN:0.0
+tmux split-window -h -p 65 -t TRAIN:0.2
+tmux send-keys -t TRAIN:0.2 'watch -n0.5 nvidia-smi' C-m
+tmux send-keys -t TRAIN:0.3 'htop' C-m
+tmux new-window -n console -t TRAIN
 tmux attach -t TRAIN:0.0
