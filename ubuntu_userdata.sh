7c852f43c5101b037e100da46b48a265cdb7da30
Josiah Davis <josiah.j.davis@gmail.com>
2020-01-22 13:35:06 +0800

    Update ubuntu_userdata.sh

--- ubuntu_userdata.sh
+++ ubuntu_userdata.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 set -e -x
-curl -o /home/ubuntu/.tmux.conf https://raw.githubusercontent.com/josiahdavis/aws-reference/master/.tmux.conf
-curl -o /home/ubuntu/start_tmux.sh https://raw.githubusercontent.com/josiahdavis/aws-reference/master/start_tmux.sh
+
+# Configure tmux
+curl https://raw.githubusercontent.com/josiahdavis/aws-reference/master/.tmux.conf -o /home/ubuntu/.tmux.conf
+curl https://raw.githubusercontent.com/josiahdavis/aws-reference/master/start_tmux.sh -o /bin/start_tmux
+chmod +x /bin/start_tmux
+
+# Configure vim
 pip3 install black
 git clone https://github.com/VundleVim/Vundle.vim.git /home/ubuntu/.vim/bundle/Vundle.vim
-curl -o /home/ubuntu/.vimrc https://raw.githubusercontent.com/josiahdavis/aws-reference/master/.vimrc
+curl https://raw.githubusercontent.com/josiahdavis/aws-reference/master/.vimrc -o /home/ubuntu/.vimrc
 chown -R ubuntu:ubuntu /home/ubuntu/.vim
 chown ubuntu:ubuntu /home/ubuntu/.vimrc
 runuser -l  ubuntu -c 'vim +PluginInstall +qall'
+
+# Configure bash
+curl https://raw.githubusercontent.com/josiahdavis/aws-reference/master/.bash_aliases -o /home/ubuntu/.bash_aliases
+
+# Additional packages
 apt install ctags
 apt install ack
 apt install tree
