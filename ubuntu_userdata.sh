cdb04c7c8f2baf9d127a6a713a381d97167481b2
Josiah Davis <josiah.j.davis@gmail.com>
2020-02-12 09:56:57 +0800

    Configure vim before tmux

--- ubuntu_userdata.sh
+++ ubuntu_userdata.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -e -x
 
-# Configure tmux
-runuser -l  ubuntu -c 'git clone https://github.com/jimeh/tmux-themepack.git /home/ubuntu/.tmux-themepack'
-runuser -l  ubuntu -c 'curl https://raw.githubusercontent.com/josiahdavis/aws-reference/master/.tmux.conf -o /home/ubuntu/.tmux.conf'
-curl https://raw.githubusercontent.com/josiahdavis/aws-reference/master/start_tmux.sh -o /bin/start_tmux
-chmod +x /bin/start_tmux
-
 # Configure vim
 pip3 install black
 git clone https://github.com/VundleVim/Vundle.vim.git /home/ubuntu/.vim/bundle/Vundle.vim
@@ -14,6 +8,12 @@ curl https://raw.githubusercontent.com/josiahdavis/aws-reference/master/.vimrc -
 chown -R ubuntu:ubuntu /home/ubuntu/.vim
 chown ubuntu:ubuntu /home/ubuntu/.vimrc
 runuser -l  ubuntu -c 'vim +PluginInstall +qall'
+
+# Configure tmux
+runuser -l  ubuntu -c 'git clone https://github.com/jimeh/tmux-themepack.git /home/ubuntu/.tmux-themepack'
+runuser -l  ubuntu -c 'curl https://raw.githubusercontent.com/josiahdavis/aws-reference/master/.tmux.conf -o /home/ubuntu/.tmux.conf'
+curl https://raw.githubusercontent.com/josiahdavis/aws-reference/master/start_tmux.sh -o /bin/start_tmux
+chmod +x /bin/start_tmux
 
 # Configure bash
 curl https://raw.githubusercontent.com/josiahdavis/aws-reference/master/.bash_aliases -o /home/ubuntu/.bash_aliases
