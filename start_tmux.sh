533a6e07356bd20538aa2647cf8c2a8067cda3c6
Josiah Davis <josiah.j.davis@gmail.com>
2020-02-27 10:18:46 +0800

    htop

--- start_tmux.sh
+++ start_tmux.sh
@@ -4,6 +4,7 @@ tmux send-keys -t TRAIN:0 'source activate python3' C-m
 tmux new-window -n MONITOR -t TRAIN
 tmux send-keys -t TRAIN:1 'watch -n0.5 nvidia-smi' C-m
 tmux split-window -h -p 60 -t TRAIN:1
+tmux send-keys -t TRAIN:1.1 'htop' C-m
 tmux new-window -n JUPYTER -t TRAIN
 tmux send-keys -t TRAIN:3 'source activate python3' C-m
 tmux attach -t TRAIN:0
