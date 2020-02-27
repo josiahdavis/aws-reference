e7bf6621521a9b485a4a988fc71ca38e5320c137
Josiah Davis <josiah.j.davis@gmail.com>
2020-02-27 10:05:50 +0800

    more windows and less panes

--- start_tmux.sh
+++ start_tmux.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-tmux new-session -s TRAIN -n editor -d
-tmux split-window -v -p 40 -t TRAIN
-tmux split-window -h -p 65 -t TRAIN:0.0
-tmux split-window -h -p 65 -t TRAIN:0.2
-tmux send-keys -t TRAIN:0.2 'watch -n0.5 nvidia-smi' C-m
-tmux send-keys -t TRAIN:0.3 'htop' C-m
-tmux new-window -n console -t TRAIN
-tmux attach -t TRAIN:0.0
+tmux new-session -s TRAIN -n EDITOR -d
+tmux send-keys -t TRAIN:0 'source activate python3' C-m
+tmux new-window -n MONITOR -t TRAIN
+tmux send-keys -t TRAIN:1 'watch -n0.5 nvidia-smi' C-m
+tmux split-window -h -p 60 -t TRAIN:1
+tmux new-window -n JUPYTER -t TRAIN
+tmux send-keys -t TRAIN:3 'source activate python3' C-m
+tmux attach -t TRAIN:0
