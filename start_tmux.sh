965f3d6f0b67a827dac1e0405429c9e3c8b6a35a
Josiah Davis <josiah.j.davis@gmail.com>
2020-03-05 16:42:08 +0800

    using pytorch_p36 venv for time being

--- start_tmux.sh
+++ start_tmux.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 tmux new-session -s TRAIN -n EDITOR -d
-tmux send-keys -t TRAIN:0 'source activate python3' C-m
+tmux send-keys -t TRAIN:0 'source activate pytorch_p36' C-m
 tmux new-window -n MONITOR -t TRAIN
 tmux send-keys -t TRAIN:1 'watch -n0.5 nvidia-smi' C-m
 tmux split-window -h -p 60 -t TRAIN:1
 tmux send-keys -t TRAIN:1.1 'htop' C-m
 tmux new-window -n JUPYTER -t TRAIN
-tmux send-keys -t TRAIN:3 'source activate python3' C-m
+tmux send-keys -t TRAIN:2 'source activate pytorch_p36' C-m
 tmux attach -t TRAIN:0
