38ac010cf6dfcc8c58fbbe1e2c2709309bde26a4
Josiah Davis <josiah.j.davis@gmail.com>
2020-03-11 11:20:44 +0800

    abbreviated start_tmux --> stm

--- ubuntu_userdata.sh
+++ ubuntu_userdata.sh
@@ -12,8 +12,8 @@ runuser -l  ubuntu -c 'vim +PluginInstall +qall'
 # Configure tmux
 runuser -l  ubuntu -c 'git clone https://github.com/jimeh/tmux-themepack.git /home/ubuntu/.tmux-themepack'
 runuser -l  ubuntu -c 'curl https://raw.githubusercontent.com/josiahdavis/aws-reference/master/.tmux.conf -o /home/ubuntu/.tmux.conf'
-curl https://raw.githubusercontent.com/josiahdavis/aws-reference/master/start_tmux.sh -o /bin/start_tmux
-chmod +x /bin/start_tmux
+curl https://raw.githubusercontent.com/josiahdavis/aws-reference/master/start_tmux.sh -o /bin/stm
+chmod +x /bin/stm
 
 # Configure bash
 curl https://raw.githubusercontent.com/josiahdavis/aws-reference/master/.bash_aliases -o /home/ubuntu/.bash_aliases
