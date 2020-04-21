0ada165b53a1ebb98068789f3b016d77eb694d75
Josiah Davis <josiah.j.davis@gmail.com>
2020-04-21 06:31:04 +0000

    calling home dir

--- ubuntu_userdata.sh
+++ ubuntu_userdata.sh
@@ -2,16 +2,16 @@
 set -e -x
 
 # Download and link dot files
-runuser -l ubuntu -c 'git clone https://github.com/josiahdavis/dotfiles.git /home/ubuntu/dotfiles'
-runuser -l ubuntu -c 'bash /home/ubuntu/dotfiles/link_dot.sh'
+runuser -l ubuntu -c 'git clone https://github.com/josiahdavis/dotfiles.git $HOME/dotfiles'
+runuser -l ubuntu -c 'bash $HOME/dotfiles/link_dot.sh'
 
 # Configure vim
 pip3 install black
-runuser -l ubuntu -c 'git clone https://github.com/VundleVim/Vundle.vim.git /home/ubuntu/.vim/bundle/Vundle.vim'
+runuser -l ubuntu -c 'git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim'
 runuser -l  ubuntu -c 'vim +PluginInstall +qall'
 
 # Configure tmux
-runuser -l  ubuntu -c 'git clone https://github.com/jimeh/tmux-themepack.git /home/ubuntu/.tmux-themepack'
+runuser -l  ubuntu -c 'git clone https://github.com/jimeh/tmux-themepack.git $HOME/.tmux-themepack'
 curl https://raw.githubusercontent.com/josiahdavis/aws-reference/master/start_tmux.sh -o /bin/stm
 chmod +x /bin/stm
 
