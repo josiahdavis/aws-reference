d6c5d0f5e4be0cd9b1955630e65eaef0d763ea69
Josiah Davis <josiah.j.davis@gmail.com>
2020-06-12 10:56:48 +0800

    Change monitor pane from h to v split

--- start_tmux.sh
+++ start_tmux.sh
@@ -3,7 +3,7 @@ tmux new-session -s TRAIN -n EDITOR -d
 tmux send-keys -t TRAIN:0 'source activate pytorch_p36' C-m
 tmux new-window -n MONITOR -t TRAIN
 tmux send-keys -t TRAIN:1 'watch -n0.5 nvidia-smi' C-m
-tmux split-window -h -p 60 -t TRAIN:1
+tmux split-window -v -p 60 -t TRAIN:1
 tmux send-keys -t TRAIN:1.1 'htop' C-m
 tmux new-window -n JUPYTER -t TRAIN
 tmux send-keys -t TRAIN:2 'source activate pytorch_p36' C-m
