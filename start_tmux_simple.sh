fa0336108877ef04d3c88bb11f69ae28c91df209
Josiah Davis <josiah.j.davis@gmail.com>
2020-12-03 14:47:27 +0800

    Add start to jupyter notebook in 2nd window

--- start_tmux_simple.sh
+++ start_tmux_simple.sh
@@ -3,4 +3,5 @@ tmux new-session -s DEV -n EDITOR -d
 tmux send-keys -t DEV:0 'source activate python3' C-m
 tmux new-window -n JUPYTER -t DEV
 tmux send-keys -t DEV:1 'source activate python3' C-m
-tmux attach -t DEV:0
+tmux send-keys -t DEV:1 'jupyter notebook' C-m
+tmux attach -t DEV
