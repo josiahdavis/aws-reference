9d375cdad36adf15313f52628fd97ed738f49098
Josiah Davis <josiah.j.davis@gmail.com>
2020-12-03 14:09:13 +0800

    Add new tmux startup with two windows only

---
+++ start_tmux_simple.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+tmux new-session -s DEV -n EDITOR -d
+tmux send-keys -t DEV:0 'source activate python3' C-m
+tmux new-window -n JUPYTER -t DEV
+tmux send-keys -t DEV:1 'source activate python3' C-m
+tmux attach -t DEV:0
