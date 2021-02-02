93b9c83c238e61259b9f005bf2fbc6f62a2a9467
Josiah Davis <josiah.j.davis@gmail.com>
2021-02-02 17:09:33 +0800

    Start tmux simple without jupyter notebook but with split screen

--- start_tmux_simple.sh
+++ start_tmux_simple.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 tmux new-session -s DEV -n EDITOR -d
 tmux send-keys -t DEV:0 'source activate python3' C-m
-tmux new-window -n JUPYTER -t DEV
-tmux send-keys -t DEV:1 'source activate python3' C-m
-tmux send-keys -t DEV:1 'jupyter notebook' C-m
+tmux split-window -v -p 30 -t DEV:0
 tmux attach -t DEV
