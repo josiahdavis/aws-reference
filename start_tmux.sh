e3b9ff8be83c96857d96ba9d5638ec4359ba1ae8
EC2 Default User <ec2-user@ip-172-31-16-142.ap-southeast-1.compute.internal>
2021-02-11 09:45:24 +0000

    Make virtual environment a parameter

--- start_tmux.sh
+++ start_tmux.sh
@@ -6,38 +6,40 @@
 #   ./stm.sh    # single window with two panes, one for editing code, one for execution.
 #   ./stm.sh -j # two windows, the second one for a jupyter notebook.
 #   ./stm.sh -m # two windows, the second one for monitoring with htop.
-#   ./stm.sh -j -m # three windows, see above
+#   ./stm.sh -n # two windows, the second one for monitoring with htop and nvidia-smi
+
+
+CENV='python3'
 
 tmux has-session -t DEV 2>/dev/null
 if [ "$?" -eq 1 ] ; then
     # No session found
     echo "Starting tmux session $@"
-    COUNT=1
+    i=1
     tmux new-session -s DEV -n DEV -d
-    tmux send-keys -t DEV:0 'source activate python3' C-m
+    tmux send-keys -t DEV:0 "source activate ${CENV}; clear" C-m
     tmux split-window -v -p 30 -t DEV:0
-    tmux send-keys -t DEV:0.1 'source activate python3' C-m
+    tmux send-keys -t DEV:0.1 "source activate ${CENV}; clear" C-m
 
-    for i in "$@"; do
-        case $i in 
+    for ARG in "$@"; do
+        case $ARG in 
         "-j" )
-            tmux new-window -n JUPYTER -t DEV:${COUNT}
-            tmux send-keys -t DEV:${COUNT} 'source activate python3' C-m
-            tmux send-keys -t DEV:${COUNT} 'jupyter notebook' C-m
+            tmux new-window -n JUPYTER -t DEV:$i
+            tmux send-keys -t DEV:$i "source activate ${CENV}; jupyter notebook" C-m
             ;;
         "-m" )
-            tmux new-window -n MONITOR -t DEV:${COUNT}
-            tmux send-keys -t DEV:${COUNT} 'htop' C-m
+            tmux new-window -n MONITOR -t DEV:$i
+            tmux send-keys -t DEV:$i 'htop' C-m
             ;;
         "-n" )
-            tmux new-window -n MONITOR -t DEV:${COUNT}
-            tmux send-keys -t DEV:${COUNT} 'watch -n0.5 nvidia-smi' C-m
-            tmux split-window -v -p 60 -t DEV:${COUNT}
-            tmux send-keys -t DEV:${COUNT}.1 'htop' C-m
+            tmux new-window -n MONITOR -t DEV:$i
+            tmux send-keys -t DEV:$i 'watch -n0.5 nvidia-smi' C-m
+            tmux split-window -v -p 60 -t DEV:$i
+            tmux send-keys -t DEV:${i}.1 'htop' C-m
             ;;
         *) echo "The option $i is not supported"
         esac
-        let COUNT=$COUNT+1
+        let i=$i+1
     done
 fi
 tmux attach -t DEV:0
